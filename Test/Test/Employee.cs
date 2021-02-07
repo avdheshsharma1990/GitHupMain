d5574683a4fb6ea873258280c0d256425309072b
avdheshsharma1990 <78697573+avdheshsharma1990@users.noreply.github.com>
2021-02-07 21:53:57 +0100

    Updated employees file

--- Test/Test/Employee.cs
+++ Test/Test/Employee.cs
@@ -8,13 +8,12 @@ namespace Test
 {
     public class Employee
     {
-        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string ID { get; set; }
         public string Salary { get; set; }
         public string Address { get; set; }
-
         public string Country { get; set; }
-
-        public string City { get; set; }
+        public string City { get; set; }        
     }
 }
